4110e69f4681d1f3732678dac3aac6c136af9f79
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-01-11 00:35:59 +0100

    add parse string test

--- test/index.js
+++ test/index.js
@@ -65,7 +65,8 @@ tape('parsers', function (t) {
       post: function (req, res, ctx, done) {
         merry.parse.json(req, function (err, json) {
           t.ifError(err, 'no error in parser')
-          t.pass('was called')
+          var expected = { foo: 'bar' }
+          t.deepEqual(json, expected, 'parsed object was cool')
           done(null, 'oi')
         })
       }
@@ -81,6 +82,33 @@ tape('parsers', function (t) {
         server.close()
       })
       req.end('{ "foo": "bar" }')
+    })
+  })
+
+  t.test('should parse text', function (t) {
+    t.plan(4)
+    var app = merry({ logStream: devnull() })
+    app.router(['/', {
+      post: function (req, res, ctx, done) {
+        merry.parse.string(req, function (err, string) {
+          t.ifError(err, 'no error in parser')
+          var expected = "now who's the funky monkey"
+          t.equal(string, expected, 'string was parsed correctly')
+          done(null, 'oi')
+        })
+      }
+    }])
+    var server = http.createServer(app.start())
+    server.listen(function () {
+      var port = getPort(server)
+      var uri = 'http://localhost:' + port + '/'
+
+      var req = request.post(uri, function (err, req) {
+        t.ifError(err, 'no err')
+        t.equal(req.statusCode, 200, 'status is ok')
+        server.close()
+      })
+      req.end("now who's the funky monkey")
     })
   })
 })
