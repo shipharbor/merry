98337fa947e2d9e521533a6d17f2b43474ca2706
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-01-10 23:58:23 +0100

    add test for json parser

--- test/index.js
+++ test/index.js
@@ -1,18 +1,18 @@
 var getPort = require('get-server-port')
 var devnull = require('dev-null')
 var request = require('request')
-var test = require('tape')
+var tape = require('tape')
 var http = require('http')
 var merry = require('../')
 
-test('merry()', function (t) {
+tape('merry()', function (t) {
   t.test('should assert input types', function (t) {
     t.plan(1)
     t.throws(merry.bind(null, 'oi'), /object/)
   })
 })
 
-test('http handlers', function (t) {
+tape('http handlers', function (t) {
   t.test('should handle GET requests by default', function (t) {
     t.plan(3)
     var app = merry({ logStream: devnull() })
@@ -53,6 +53,34 @@ test('http handlers', function (t) {
         t.equal(req.statusCode, 200, 'status is ok')
         server.close()
       })
+    })
+  })
+})
+
+tape('parsers', function (t) {
+  t.test('should parse JSON', function (t) {
+    t.plan(4)
+    var app = merry({ logStream: devnull() })
+    app.router(['/', {
+      post: function (req, res, ctx, done) {
+        merry.parse.json(req, function (err, json) {
+          t.ifError(err, 'no error in parser')
+          t.pass('was called')
+          done(null, 'oi')
+        })
+      }
+    }])
+    var server = http.createServer(app.start())
+    server.listen(function () {
+      var port = getPort(server)
+      var uri = 'http://localhost:' + port + '/'
+
+      var req = request.post(uri, function (err, req) {
+        t.ifError(err, 'no err')
+        t.equal(req.statusCode, 200, 'status is ok')
+        server.close()
+      })
+      req.end('{ "foo": "bar" }')
     })
   })
 })
