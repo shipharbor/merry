3082b03f9c5ede62a62bb61c63c5a97432f06210
lrlna <shestak.irina@gmail.com>
2017-06-02 13:11:39 +0200

    server router returns a params object

--- example.js
+++ example.js
@@ -11,6 +11,12 @@ app.route('GET', '/', function (req, res, ctx) {
   ctx.send(200, { cute: 'butts' })
 })
 
+app.route('GET', '/search/:query', function (req, res, ctx) {
+  ctx.log.info('oh hey, a request here')
+  ctx.log.info(ctx.params.query) // logs a :query param
+  ctx.send(200, { cute: 'butts' })
+})
+
 app.route('default', function (req, res, ctx) {
   ctx.log.info('Route doesnt exist')
   ctx.send(404, { message: 'nada butts here' })
